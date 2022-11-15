bee4b9e734dba564d0d825afb602a2d09792bea8
raaka <dihrando@gmail.com>
2022-11-15 08:14:09 -0500

    My very first commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,4 +1,5 @@
  #! /bin/bash
 
- echo "7719"
- curl -s https://academy.digifemmes.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"raaka\"}}){id}}"}'
+ echo "raaka"
+ curl -s https://academy.digifemmes.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"raaka\"}}){id}}"}
+ 
