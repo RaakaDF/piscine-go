32fe141da20552fc504aa9692f823cb402d1c76e
raaka <dihrando@gmail.com>
2022-11-15 10:09:30 -0500

    My very second commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,3 +1,1 @@
- #! /bin/bash
-
- echo "7719"
+curl -s https://academy.digifemmes.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"raaka\"}}){id}}"}'|jq '.data.user[0].id'
