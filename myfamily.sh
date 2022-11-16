64bcbe551b9664bcd4f45f25362bb6a8a92847b3
raaka <dihrando@gmail.com>
2022-11-16 04:03:48 -0500

    My very seventh commit

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #! \bin\bash
 
- curlhttps://academy.digifemmes.com/assets/superhero/all.json | jq --argjson id "$HERO_ID" '. [] | select(.id == $id) | .connections.relatives' | sed 's/^"\(.*\)"$/\1/'
+ curl https://academy.digifemmes.com/assets/superhero/all.json | jq --argjson id "$HERO_ID" '. [] | select(.id == $id) | .connections.relatives' | sed 's/^"\(.*\)"$/\1/'
