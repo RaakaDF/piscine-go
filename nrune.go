b8770be8b0fa57795cad42e1ad2f774ef269acb3
raaka <dihrando@gmail.com>
2022-11-22 12:18:15 -0500

    nrune.go

--- nrune.go
+++ nrune.go
@@ -1,4 +1,4 @@
-package main
+package piscine
 
 func NRune(s string, n int) rune {
 	x := []rune(s)
@@ -9,12 +9,3 @@ func NRune(s string, n int) rune {
 		return x[n-1]
 	}
 }
-
-//func main() {
-//	z01.PrintRune(NRune("Hello!", 3))
-//	z01.PrintRune(NRune("Salut!", 2))
-//	z01.PrintRune(NRune("Bye!", -1))
-//	z01.PrintRune(NRune("Bye!", 5))
-//	z01.PrintRune(NRune("Ola!", 4))
-//	z01.PrintRune('\n')
-//}
