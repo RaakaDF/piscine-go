ce1fc231151fc816c3424f495d2e966185b695a3
Gnandji Christelle Manuela Kouassi <raaka@noreply.academy.digifemmes.com>
2022-11-25 16:59:47 +0000

    Add 'basicatoi.go'

---
+++ basicatoi.go
@@ -0,0 +1,25 @@
+
+package piscine
+
+
+func BasicAtoi(s string) int {
+	byteRevStr := []byte(s)
+	lengthStr := -1
+
+	for range s {
+		lengthStr++
+	}
+
+	for index := range s {
+		byteRevStr[lengthStr-index] = s[index]
+		byteRevStr[lengthStr-index] -= 48
+	}
+
+	num := 0
+	tenth := 1
+	for index := range s {
+		num = num + (int(byteRevStr[index]) * tenth)
+		tenth *= 10
+	}
+	return num
+}
