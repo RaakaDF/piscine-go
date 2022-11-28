433ba6b5f7ee5ff04fc07e944580977bd426089f
Gnandji Christelle Manuela Kouassi <raaka@noreply.academy.digifemmes.com>
2022-11-28 17:24:04 +0000

    Update 'advancedsortwordarr.go'

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -1,10 +1,12 @@
 package piscine
+
 // AdvancedSortWordArr sort a word characters in ascending order
 func AdvancedSortWordArr(a []string, f func(a, b string) int) {
 	lena := 0
 	for i := range a {
 		lena = i + 1
 	}
+	
 	for i := 0; i < lena-1; i++ {
 		for j := i + 1; j < lena; j++ {
 			if f(a[i], a[j]) > 0 {
