ec1068dd4bf965cd36189c3fcc197cbb799a0e45
Gnandji Christelle Manuela Kouassi <raaka@noreply.academy.digifemmes.com>
2022-11-28 17:30:48 +0000

    Update 'advancedsortwordarr.go'

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -6,7 +6,6 @@ func AdvancedSortWordArr(a []string, f func(a, b string) int) {
 	for i := range a {
 		lena = i + 1
 	}
-	
 	for i := 0; i < lena-1; i++ {
 		for j := i + 1; j < lena; j++ {
 			if f(a[i], a[j]) > 0 {
