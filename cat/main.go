989b7eefabfcc697e4048f72ce02d7e5eaea92c0
raaka <dihrando@gmail.com>
2022-11-28 09:53:43 -0500

    my program cat

--- cat/main.go
+++ cat/main.go
@@ -1,48 +1,38 @@
 package main
 
 import (
+	"io"
 	"io/ioutil"
 	"os"
 
 	"github.com/01-edu/z01"
 )
 
-func put(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
+func printStr(s string) {
+	sRune := []rune(s)
+	for i := 0; i < len(sRune); i++ {
+		z01.PrintRune(sRune[i])
 	}
-}
-
-func putln(s string) {
-	put(s)
-	z01.PrintRune('\n')
-}
-
-func fatal(s string) {
-	putln(s)
-	os.Exit(1)
 }
 
 func main() {
-	i := 0
-	for range os.Args {
-		i++
-	}
-	if i == 1 {
-		b, err := ioutil.ReadAll(os.Stdin)
-		if err != nil {
-			fatal(err.Error())
-		}
-		put(string(b))
-	} else {
-		for _, file := range os.Args[1:] {
-			b, err := ioutil.ReadFile(file)
+	arg := os.Args
+	files := arg[1:]
+	if len(files) > 0 {
+		for i := 0; i < len(files); i++ {
+			content, err := ioutil.ReadFile(files[i])
 			if err != nil {
-				fatal("ERROR: " + err.Error())
+				errFile := "ERROR: open" + string(files[i]) + ": no such file or directory"
+				printStr(errFile)
+				z01.PrintRune('\n')
+				os.Exit(1)
 			}
-			put(string(b))
-			z01.PrintRune('\n')
+			printStr(string(content))
 
+		}
+	} else {
+		if _, err := io.Copy(os.Stdout, os.Stdin); err != nil {
+			panic(err)
 		}
 	}
 }
