26cf7bdce69559e00a65cfb3721cd6dfc5f2a7e3
Gnandji Christelle Manuela Kouassi <raaka@noreply.academy.digifemmes.com>
2022-11-30 14:30:40 +0000

    Update 'comcheck/main.go'

--- comcheck/main.go
+++ comcheck/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
     x := os.Args[1:]
     for _, v := range x {
-        if x == '01' || x == "galaxy" || x == "galaxy 01" {
+        if x == "01" || x == "galaxy" || x == "galaxy 01" {
             fmt.Println("Alert!!!")
             return
         }
