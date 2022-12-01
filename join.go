7e9ad9d30320670ca6c399b10e71f0706a4eee10
Gnandji Christelle Manuela Kouassi <raaka@noreply.academy.digifemmes.com>
2022-12-01 06:10:09 +0000

    Add 'join.go'

---
+++ join.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Join(strs []string, sep string) string {
+	result := ""
+	lenstrs := 0
+	for i := range strs {
+		lenstrs = i + 1
+	}
+
+	for i := range strs {
+		if i != lenstrs-1 {
+			result = result + strs[i] + sep
+		} else {
+			result = result + strs[i]
+		}
+	}
+	return result
+}
