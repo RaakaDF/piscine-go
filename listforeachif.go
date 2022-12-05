bb3a6436c2651f655f3663ae3e48433916dcf79c
Gnandji Christelle Manuela Kouassi <raaka@noreply.academy.digifemmes.com>
2022-12-05 10:05:15 +0000

    Add 'listforeachif.go'

---
+++ listforeachif.go
@@ -0,0 +1,42 @@
+package piscine
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+
+	num := l.Head
+	for num != nil {
+		if cond(num) {
+			f(num)
+		}
+		num = num.Next
+	}
+}
+
+func IsPositive_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNegative_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNotNumeric_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return false
+	case string, rune:
+		return true
+	}
+	return true
+}
