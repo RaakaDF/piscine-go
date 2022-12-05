190869a5897a6803cc737d21b3583fcaa398606c
Gnandji Christelle Manuela Kouassi <raaka@noreply.academy.digifemmes.com>
2022-12-05 08:36:18 +0000

    Update 'listpushback.go'

--- listpushback.go
+++ listpushback.go
@@ -12,7 +12,7 @@ type List struct {
 
 func ListPushFront(l *List, data interface{}) {
 	if l.Head == nil {
-		l.Head, l.Tail = &NodeL{Data:data}, l.Head
+		l.Head, l.Tail = &NodeL{Data: data}, l.Head
 	}else{
 		newNode := &NodeL{Data: data}
 		newNode.Next,l.Head = l.Head, newNode
